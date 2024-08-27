86bdae415fcc7dc023318ed54df94fd346ed9bad
dinesh <dineshsutharinfo@gmail.com>
2024-08-27 09:17:57 +0530

    vercel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 import LaunchTracker from './LaunchTracker';
 
 function App() {
   return (
-    <div className="App">
+    <div>
         <LaunchTracker />
     </div>
   );
